fa218c8cdcce2ce02d37d205fb80d05c8e48c00f
Pratmika Shridhar Nayak <pratmikashridharnayak@Pratmikas-Mac-mini.local>
2017-09-05 18:22:42 +0530

    Initial Commit

---
+++ ControllerAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControllerAssignment
+//
+//  Created by Pratmika Shridhar Nayak on 05/09/17.
+//  Copyright © 2017 Pratmika Shridhar Nayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
